c8cba505f26d9b106f5cefa32d5bc6c4cfe79547
jitendrarik <jitendra@binarynumbers.io>
2023-08-20 22:14:40 +0530

    latest

--- src/views/pages/Home/afterScanDetails.vue
+++ src/views/pages/Home/afterScanDetails.vue
@@ -1,50 +1,70 @@
 <template>
   <div>
-    <b-row>
-      <b-col cols="12">
-        <div>
-          <div>
-            <div class="panel panel-info">
-              <div class="panel-heading m-2">
-                <div>
-                  <b-col>
-                    <b-row
-                      v-for="(imgItem, imgIndex) in userDetailsArray"
-                      :key="imgIndex"
-                      v-if="imgItem.inputType == 'file'"
-                    >
-                      <b-col>
-                        <b-img
-                          v-if="imgItem.vModelValue"
-                          width="150"
-                          height="150"
-                          fluid
-                          :src="BASE_URL + imgItem.vModelValue"
-                          alt="Image"
-                        ></b-img>
-                        <b-img
-                          width="150"
-                          height="150"
-                          rounded="circle"
-                          v-else
-                          class="m-1"
-                          fluid
-                          :src="imgAvtar"
-                          alt="Image 1"
-                        ></b-img>
+    <section style="background-color: #9de2ff">
+      <div class="container py-2">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col col-md-9 col-lg-7 col-xl-5">
+            <div class="card" style="border-radius: 15px">
+              <div class="card-body">
+                <div class="d-flex text-black">
+                  <div
+                    v-for="(imgItem, imgIndex) in userDetailsArray"
+                    :key="imgIndex"
+                    class="flex-shrink-0"
+                  >
+                    <img
+                      v-if="imgItem.key == 'photo'"
+                      :src="BASE_URL + imgItem.vModelValue"
+                      alt="Image"
+                      class="img-fluid"
+                      style="width: 150px; border-radius: 10px"
+                    />
+                  </div>
+                  <div class="flex-grow-1 ms-3 ml-1">
+                    <b-row>
+                      <b-col
+                        cols="12"
+                        v-for="(imgItem1, imgIndex1) in userDetailsArray"
+                        :key="imgIndex1"
+                      >
+                        <h5 v-if="imgItem1.key == 'name'" class="">
+                          <b>{{ imgItem1.vModelValue }}</b>
+                        </h5>
+                        <h6
+                          v-if="imgItem1.key == 'dob'"
+                          class=""
+                          style="color: #2b2a2a"
+                        >
+                          {{ imgItem1.vModelValue }}
+                        </h6>
+                        <h6
+                          v-if="imgItem1.key == 'gender'"
+                          class=""
+                          style="color: #2b2a2a"
+                        >
+                          {{ imgItem1.vModelValue }}
+                        </h6>
                       </b-col>
                     </b-row>
-                  </b-col>
+                  </div>
                 </div>
               </div>
-              <div>
-                <div>
+            </div>
+            <div class="card" style="border-radius: 15px">
+              <div class="card-body">
+                <div class="d-flex text-black">
                   <div>
-                    <table class="table">
+                    <table>
                       <tbody>
                         <tr
                           v-for="(item, index) in userDetailsArray"
                           :key="index"
+                          v-if="
+                            item.key != 'photo' &&
+                            item.key != 'name' &&
+                            item.key != 'dob' &&
+                            item.key != 'gender'
+                          "
                         >
                           <td
                             align="left"
@@ -61,80 +81,9 @@
                               {{ item.vModelValue }}
                             </div>
                           </td>
-                          <!-- <td
-                            align="left"
-                            style="text-align: left"
-                            v-if="item.inputType != 'file'"
-                          >
-                          
-                            <div
-                              style="text-align: left"
-                              v-if="item.inputType == 'quill'"
-                              v-html="item.vModelValue"
-                            ></div>
-                            <div style="text-align: left" v-else>
-                              {{ item.vModelValue }}
-                            </div>
-                          </td> -->
                         </tr>
                       </tbody>
                     </table>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </b-col>
-    </b-row>
-    <section class="vh-100" style="background-color: #9de2ff">
-      <div class="container py-5 h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-          <div class="col col-md-9 col-lg-7 col-xl-5">
-            <div class="card" style="border-radius: 15px">
-              <div class="card-body p-4">
-                <div class="d-flex text-black">
-                  <div class="flex-shrink-0">
-                    <img
-                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
-                      alt="Generic placeholder image"
-                      class="img-fluid"
-                      style="width: 180px; border-radius: 10px"
-                    />
-                  </div>
-                  <div class="flex-grow-1 ms-3">
-                    <h5 class="mb-1">Danny McLoan</h5>
-                    <p class="mb-2 pb-1" style="color: #2b2a2a">
-                      Senior Journalist
-                    </p>
-                    <div
-                      class="d-flex justify-content-start rounded-3 p-2 mb-2"
-                      style="background-color: #efefef"
-                    >
-                      <div>
-                        <p class="small text-muted mb-1">Articles</p>
-                        <p class="mb-0">41</p>
-                      </div>
-                      <div class="px-3">
-                        <p class="small text-muted mb-1">Followers</p>
-                        <p class="mb-0">976</p>
-                      </div>
-                      <div>
-                        <p class="small text-muted mb-1">Rating</p>
-                        <p class="mb-0">8.5</p>
-                      </div>
-                    </div>
-                    <div class="d-flex pt-1">
-                      <button
-                        type="button"
-                        class="btn btn-outline-primary me-1 flex-grow-1"
-                      >
-                        Chat
-                      </button>
-                      <button type="button" class="btn btn-primary flex-grow-1">
-                        Follow
-                      </button>
-                    </div>
                   </div>
                 </div>
               </div>
